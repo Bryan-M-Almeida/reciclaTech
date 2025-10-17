4b2d900faa5c215894a3f1c3d89bf9e864df1ce4
Bryan <bryanmacieldealmeida06@gmail.com>
2025-10-17 15:45:49 -0300

    Feed: Menu mobile inicial, ajustes leves na responsividade, ajustes em cores para melhor harmonia

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const logoAnimate = document.querySelector('#logoAnimate');
 logoAnimate.addEventListener('click', () => {
     logoAnimate.classList.toggle('rotate-180');
 })
+
+const menuBar = document.querySelector('#menuMobile');
+const menuContainer = document.querySelector('#menuContainer');
+let isOpen = false;
+menuBar.addEventListener('click', () => {
+    if (menuBar.classList.contains('fa-bars') && isOpen == false) {
+        menuBar.classList.remove('fa-bars');
+        menuBar.classList.add('fa-remove');
+        menuContainer.classList.toggle('hidden')
+        isOpen = true;
+    } else if (menuBar.classList.contains('fa-remove') && isOpen == true) {
+        menuBar.classList.remove('fa-remove');
+        menuBar.classList.add('fa-bars');
+        menuContainer.classList.toggle('hidden')
+        isOpen = false;
+    }
+})
